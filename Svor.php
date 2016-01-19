113566525a42974a993346659fa35dfeeb4eed18
unknown <1212962259@TV620-08.2t.local>
2016-01-19 09:12:57 +0000

    Uploading inital draft

---
+++ Svor.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Author: Valdimar Gunnarsson
+ *
+ * 1. HTTP stendur fyrir Hypertext Transfer Protocol
+ *    HTTP er stateless protocol, sem þýðir að hann lítur á hver og eina beiðni sem einstaka færslu og geymir aldrei neina upplýsingar um notandann.
+ *    Til að geyma upplýsingar um notanda er yfirleitt notaðar 'cookies'
+ *
+ * 2. 
+ *
+ * */
