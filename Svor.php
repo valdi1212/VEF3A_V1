da2183232a8c5799b483fb85163a8980df212ceb
unknown <1212962259@TV620-08.2t.local>
2016-01-20 12:24:23 +0000

    Uploading work done in class

--- Svor.php
+++ Svor.php
@@ -39,5 +39,23 @@
  *    502: 'Bad Gateway' -- Þessi kóði getur oft komið þegar það er vandamál með tengin á milli vefþjóns og notanda, oft vegna eldveggs
  *    503: 'Service Unavailable' -- Vefþjónn er annaðhvort of upptekinn til að höndla beiðnina eða hann er niðri í bili
  *
- * 3. 
+ * 3. HTTP 2.0 var gert vegna þess að með tímanum hefur magn af gögnum og skjölum sem þarf að nota í vefsíðum verið að hækka og var HTTP 1 ekki hannað með það í huga.
+ *    Einn af helstu mununum er það að í staðinn fyrir að nota texta fyrir protocola notar HTTP 2.0 binary sem er minna í stærð og mikið ólíklegra að villur komi upp.
+ *    Annar munur er það að HTTP 2.0 er 'multiplexað' sem gerir það að verkum að það er hæght að vinna í meira en einni beiðni frá notanda í einu, sem eykur vinnsluhraða.
+ *    HTTP 2.0 munt einungis nota eina TCP tengingu í staðinn fyrir 4-8 fyrir hvert 'origin'. Þar sem að síður nota of mörg origin er ekki ólíklegt að ein síða gæti opnað 15-30 TCP tengingar.
+ *    Þetta hættir á að bufferarnir munu fyllast af þeim og hægja á öllu, sem aftur á móti gerir það að verkum að sumum gögnum verður hent og það þarf þá að ná í þau aftur.
+ *    HTTP 2.0 mun líka nota 'header compression' til að þjappa gögnum sem þarf að skila. Samkvæmkt Patrick McManus sem vinnur fyrir Mozilla, þá tekur það um 7 ferðir að koma gögnunum af síðu sem er með um 80 assets, En ef maður þjappar headerum þá myndi það bara taka eina.
+ *
+ *
  * */
+
+$jsonData = ['events' => [
+    ['location' => 'San Francisco, CA', 'date' => 'May 1', 'map' => 'img/map-ca.png'],
+    ['location' => 'Austin, TX', 'date' => 'May 15', 'map' => 'img/map-tx.png'],
+    ['location' => 'New York, NY', 'date' => "May 30", 'map' => 'img/map-ny.png']
+]];
+
+$jsonEncoded = json_encode($jsonData);
+$jsonDecoded = json_decode($jsonEncoded);
+echo $jsonEncoded;
+echo $jsonDecoded;
